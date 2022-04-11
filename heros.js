19b82b9bffb9d6ea335d98ec0f6a275a7c61499d
Brad Slagle <btslagle@gmail.com>
2022-04-11 13:58:23 -0500

    Start over with new api

---
+++ heros.js
@@ -0,0 +1,22 @@
+const houses = document.querySelector(".houses")
+const main = document.querySelector("main")
+
+const url = ("https://akabab.github.io/superhero-api/api/all.json")
+fetch(url).then(response => {
+    return response.json()
+}).then(paresedResponse => {
+    const superHeros = paresedResponse.map(hero => hero)
+    console.log(superHeros)
+    superHeros.forEach(hero => {
+        const $heros = document.createElement("div")
+        $heros.classList.add("hero")
+        $heros.innerHTML = `
+            <h2>${hero.name}</h2>
+            <img src="${hero.images.sm}" alt "Image of ${hero.name}" />
+            <p>${hero.name} is know for working with ${hero.connections.groupAffiliation}</p>
+            <p> Alignment: ${hero.biography.alignment}</p>
+            
+            `
+        main.append($heros)
+    });
+});
