3e33594aa50801c6abcf7c8c325263f051453e3f
Brad Slagle <btslagle@gmail.com>
2022-04-19 17:21:17 -0500

    Attempt at fixing index.html

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const $form = document.querySelector("form")
-const $input = document.querySelector("#search").value
-const $submit = document.querySelector("#submit")
 const $main = document.querySelector("main")
 
 
-function getFavoriteSuper(heroOrVillian) {
+function getFavoriteSuper(favoriteSuper) {
     const $div = document.createElement("div")
-    div.innerHTML = ` Your favorite Super is ${favoriteSuper.name}
-    <a 
+    $div.classList.add("favorite")
+    div.innerHTML = ` Your favorite Super is ${favoriteSuper}
+    <a href="details.html?id=${hero.id}">${hero.name}</a>
     `
+    $main.append($div)
 }
-
-
-
-$input.addEventListener("submit", (event) => {
+$form.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const favoriteSuper = formData.get("favorite-super")
-    $submit.value = "Thank You!"
-    localStorage.setItem("favorite-super", favoriteSuper)
-
+    console.log(favoriteSuper);
+    return favoriteSuper
 })
 
-/*const url = ("https://akabab.github.io/superhero-api/api/all.json")
-fetch(url).then(response => {
-    return response.json()
-}).then(paresedResponse => {
-    const superHeroOrVillian = paresedResponse.map(heroOrVillian => heroOrVillian)
-})*/
+const url = ("https://akabab.github.io/superhero-api/api/all.json")
+fetch(url).then(response => response.json())
+    .then(paresedResponse => {
+        console.log(paresedResponse)
+        getFavoriteSuper(favoriteSuper)
+    })
