976211b7698803408285e0f1e7d011d3dd794b9c
Brad Slagle <btslagle@gmail.com>
2022-04-20 09:21:26 -0500

    Update index.js

--- heros.js
+++ heros.js
@@ -6,23 +6,23 @@ const url = ("https://akabab.github.io/superhero-api/api/all.json")
 fetch(url)
     .then(response => response.json())
     .then(paresedResponse => {
-        const superHeros = paresedResponse.map(hero => hero)
-        superHeros.forEach(hero => {
-            const $heros = document.createElement("div")
-            if (hero.biography.alignment === "good") {
-                $heros.classList.add("hero")
-                $heros.innerHTML = `
+        return paresedResponse.map(hero => hero)
+            .forEach(hero => {
+                const $heros = document.createElement("div")
+                if (hero.biography.alignment === "good") {
+                    $heros.classList.add("hero")
+                    $heros.innerHTML = `
             <h2>${hero.name}</h2>
             <img src="${hero.images.sm}" alt "Image of ${hero.name}" />
             <a href="details.html?id=${hero.id}">${hero.name}</a>
             <p>${hero.name} is know for working with ${hero.connections.groupAffiliation}</p>
             <p> Alignment: ${hero.biography.alignment}</p>
             `
-                main.append($heros)
-            }
+                    main.append($heros)
+                }
 
 
-        }).catch(error => {
-            window.location.href = "404.html"
-        })
+            }).catch((error => {
+                window.location.href = "404.html"
+            }))
     })
--- index.js
+++ index.js
@@ -2,25 +2,30 @@ const $form = document.querySelector("form")
 const $main = document.querySelector("main")
 
 
-function getFavoriteSuper(favoriteSuper) {
-    const $div = document.createElement("div")
-    $div.classList.add("favorite")
-    div.innerHTML = ` Your favorite Super is ${favoriteSuper}
-    <a href="details.html?id=${hero.id}">${hero.name}</a>
-    `
-    $main.append($div)
-}
 $form.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const favoriteSuper = formData.get("favorite-super")
-    console.log(favoriteSuper);
-    return favoriteSuper
+    getFavoriteSuper(favoriteSuper);
 })
 
-const url = ("https://akabab.github.io/superhero-api/api/all.json")
-fetch(url).then(response => response.json())
-    .then(paresedResponse => {
-        console.log(paresedResponse)
-        getFavoriteSuper(favoriteSuper)
-    })
+
+function getFavoriteSuper(favoriteSuper) {
+    fetch("https://akabab.github.io/superhero-api/api/all.json")
+        .then(response => response.json())
+        .then(paresedResponse => {
+            paresedResponse.find(response => response.name === favoriteSuper)
+            getFavoriteSuper(favoriteSuper)
+            const div = document.createElement("div")
+            $div.classList.add("favorite")
+            div.innerHTML = ` <h2> Your favorite Super is? </h2>
+            `
+            $main.append(div)
+        })
+}
+
+
+
+
+
+
