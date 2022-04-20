1ba2b4107bd11317e3b1c379f693dc92d432a2cd
Brad Slagle <btslagle@gmail.com>
2022-04-20 10:58:12 -0500

    Update index.html

--- heros.js
+++ heros.js
@@ -1,7 +1,6 @@
 const main = document.querySelector("main")
 const $search = document.querySelector(".search-wrapper")
 
-
 const url = ("https://akabab.github.io/superhero-api/api/all.json")
 fetch(url)
     .then(response => response.json())
@@ -12,16 +11,14 @@ fetch(url)
                 if (hero.biography.alignment === "good") {
                     $heros.classList.add("hero")
                     $heros.innerHTML = `
-            <h2>${hero.name}</h2>
-            <img src="${hero.images.sm}" alt "Image of ${hero.name}" />
-            <a href="details.html?id=${hero.id}">${hero.name}</a>
-            <p>${hero.name} is know for working with ${hero.connections.groupAffiliation}</p>
-            <p> Alignment: ${hero.biography.alignment}</p>
-            `
+                    <h2>${hero.name}</h2>
+                    <img src="${hero.images.sm}" alt "Image of ${hero.name}" />
+                    <a href="details.html?id=${hero.id}">${hero.name}</a>
+                    <p>${hero.name} is know for working with ${hero.connections.groupAffiliation}</p>
+                    <p> Alignment: ${hero.biography.alignment}</p>
+                    `
                     main.append($heros)
                 }
-
-
             }).catch((error => {
                 window.location.href = "404.html"
             }))
--- index.js
+++ index.js
@@ -14,18 +14,15 @@ function getFavoriteSuper(favoriteSuper) {
     fetch("https://akabab.github.io/superhero-api/api/all.json")
         .then(response => response.json())
         .then(paresedResponse => {
-            paresedResponse.find(response => response.name === favoriteSuper)
-            getFavoriteSuper(favoriteSuper)
+            const favSuper = paresedResponse.map(response => response).find(response => response.name === `${favoriteSuper}`)
             const div = document.createElement("div")
-            $div.classList.add("favorite")
-            div.innerHTML = ` <h2> Your favorite Super is? </h2>
+            div.classList.add("favorite")
+            div.innerHTML = ` <h2> Your favorite Super is ${favSuper.name}! </h2>
+            <img src = ${favSuper.images.md} alt = ${favSuper.name} />
             `
+
             $main.append(div)
+        }).catch(error => {
+            window.location.href = "404.html"
         })
 }
-
-
-
-
-
-
